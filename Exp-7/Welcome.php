da642bef651632b4d0989a1a167edeed180dbdf8
khushboodhiman <43932527+khushboodhiman@users.noreply.github.com>
2021-05-19 19:12:02 +0530

    Add files via upload

---
+++ Exp-7/Welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+
+Hello <?php 
+echo $_POST['name'];
+?> your query "<?php echo $_POST['query'] ?>"has been recorded and will be resolved asap.
+    
+</body>
+</html>
